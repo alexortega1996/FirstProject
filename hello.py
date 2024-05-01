4e1c35cbb25000307be3bb62469e04464ba59df0
alexortega1996 <62562107+alexortega1996@users.noreply.github.com>
2024-05-01 18:41:49 -0500

    feature signup working

---
+++ hello.py
@@ -0,0 +1,8 @@
+# Ask user for their name
+name = input("What's your name? ")
+
+# Remove whitespace from str and capitalize user's name
+name = name.strip().title()
+
+# Say hello to user
+print("hello, " + name)
